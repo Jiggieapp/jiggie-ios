1eef6d51ea58022c59754d445c10b377d2b97f8e
setiady <setiady@apps-foundry.com>
2015-12-15 14:02:43 +0700

    restructure project library

---
+++ PartyHost/PartyHost/Components/Analytics/AnalyticManager.h
@@ -0,0 +1,13 @@
+//
+//  AnalyticManager.h
+//  Jiggie
+//
+//  Created by Setiady Wiguna on 12/14/15.
+//  Copyright © 2015 Sunny Clark. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AnalyticManager : NSObject
+
+@end
---
+++ PartyHost/PartyHost/Components/Analytics/AnalyticManager.m
@@ -0,0 +1,13 @@
+//
+//  AnalyticManager.m
+//  Jiggie
+//
+//  Created by Setiady Wiguna on 12/14/15.
+//  Copyright © 2015 Sunny Clark. All rights reserved.
+//
+
+#import "AnalyticManager.h"
+
+@implementation AnalyticManager
+
+@end
