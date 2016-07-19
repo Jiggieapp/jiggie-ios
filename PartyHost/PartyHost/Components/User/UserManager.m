df7e36f355d16db134daae6b6ca7200cf1ebd0d2
uuds <nuruddin@jiggieapp.com>
2016-07-19 18:43:29 +0700

    Remove user defaults data when user logged out.

--- PartyHost/PartyHost/Components/User/UserManager.m
+++ PartyHost/PartyHost/Components/User/UserManager.m
@@ -88,6 +88,7 @@ static UserManager *_sharedManager = nil;
 - (void)clearAllUserData {
     NSUserDefaults *prefs = [NSUserDefaults standardUserDefaults];
     [prefs removeObjectForKey:@"INVITE_CREDIT"];
+    [prefs removeObjectForKey:@"USER_FB_ID"];
     [prefs removeObjectForKey:@"user.setting"];
     [prefs synchronize];
     
